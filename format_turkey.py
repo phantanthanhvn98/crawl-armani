6d275bc7833c37de123ea6956694508a087c098e
Joseph Lopez <josephlopez@gmail.com>
2024-04-23 16:42:25 +0700

    add reformat turkey

--- format_turkey.py
+++ format_turkey.py
@@ -7,5 +7,6 @@ df = pd.read_csv("draft_final_armaniturkiye.csv")
 df['code'] = df['name'].apply(lambda x: x.split(" | ")[-1])
 
 df = df.rename(columns={'code': 'sku'})
+df['color'] = df['color'].apply(lambda x: '#'.join(list(set(x.split("#")))))
 
 df.to_csv('final_armaniturkiye.csv', index=False)
